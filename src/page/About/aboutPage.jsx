62eba860a32b931288ac998410d1fecbef3dd5a7
hahahaday12 <hahahaday12@naver.com>
2023-05-02 04:01:23 +0900

    style: 이미지 경로 수정

--- src/page/About/aboutPage.jsx
+++ src/page/About/aboutPage.jsx
@@ -1,26 +1,22 @@
 import React from "react";
-import jjang from '../../image/jjang.jpg'
 import '../../scss/about/about.scss'
+import sell from '../../image/sell.jpg';
 
 function AboutPage () {
   return(
     <div className="AboutContainer">
       <div className="AboutContainer-photo">
-        <img src={jjang}/>
+        <img src={sell}/>
       </div>
 
       <div className="AboutContainer-box">
         <span>HAHAEUN</span>
-
-        
         <div className="AboutContainer-box__bottom">
         <p> 📨 hahahaday122@gmail.com</p>
         <p> 💻 https://github.com/hahahaday12</p>
         <p> 🎈 https://velog.io/@hahahaday12</p>
         </div>
       </div>
- 
-
     </div>
   )
 };
