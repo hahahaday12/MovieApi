0cb0c0ddcbbe3f0171c5c8f19d1d40cb41251998
hahahaday12 <hahahaday12@naver.com>
2023-05-02 04:02:51 +0900

    feat:무한 스크롤 기능 추가
    -  무한 스크롤 기능을 추가.

--- src/page/Home/components/searchresult.jsx
+++ src/page/Home/components/searchresult.jsx
@@ -1,13 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import classNames from 'classnames/bind'
 import Loading from "../../../components/loading";
 import styles from '../../../scss/home/components/SearchResult.module.scss'
+import PageContext from "../../../store/context";
+import { Skeleton } from "@mui/material";
 
-function SearchResult (props) {
+function SearchResult(props) {
 
   const navigate = useNavigate();
   const st = classNames.bind(styles)
+  const { value, setValue } = useContext(PageContext);
 
   const [param, setParam] = useState({
     title: '',
@@ -20,84 +23,131 @@ function SearchResult (props) {
   const [dataList, setDatalist] = useState([]);
   const [dataState, setDataState] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [hasNextPage, setHasNextPage] = useState(false);
+  const Pagee = useRef(1);
+  const observerTargetEl = useRef(null);
 
-  useEffect(()=>{
-    if(props.setInfo){
-        setParam({...param, ...props.setInfo});
+// 1번 입력한 데이터를 받아오고 저장
+  useEffect(() => {
+    if (props.setInfo) {
+      setParam({ ...param, ...props.setInfo });
     }
   }, [props.setInfo]);
 
+//2번 api 호출 및 기본값 설정 
   useEffect(() => {
-    console.log("param", param);
-    if(param.title){
+    if (param.title) {
+      //기본값 초기값 지정 화면에서 다른조건을 입력했을때 데이터추가방지
       setDatalist([]);
-      let max = param.number/10;
-      //max = 10->1, 20->2, 30->3
-      for(let i=1; i<max+1; i++){
-        sendApi(i);
-      }
-    }       
+      Pagee.current = 1;
+      setHasNextPage(false);
+      sendApi(Pagee.current);
+    }else{
+      setDataState(false);
+    }
   }, [param]);
 
   const sendApi = async (page) => {
-    setLoading(true)
+    if (!param.title) return false;
+    setLoading(true);
     const s = `&s=${param.title}`;
     const y = `&y=${param.year}`;
     const p = `&page=${page}`;
-
+  
     try {
       const res = await fetch(`https://omdbapi.com/?apikey=7035c60c${s}${y}${p}`);
       const json = await res.json();
       if (json.Response === "True") {
-          setLoading(false);
-          const { Search: movies } = json;
-          setDatalist(dataList => [...dataList, ...movies]); // 영화 목록을 state에 저장
-          setDataState(true); // 영화 목록이 있다는 상태를 true로 설정
-      } else {
-        setLoading(true);
-        alert(data.Error);
+        
+        const { Search: movies, totalResults: total } = json;
+        setDatalist(prevDataList => [...prevDataList, ...movies]);
+        // true 로 만들어 주기 위해 아래 식 무한스크롤을 위한 조건식 
+        setHasNextPage(Pagee.current < Math.round(total/10));
+        // 데이터 유무 
+        setDataState(true);
         setLoading(false);
+        
+      } else {
+        setLoading(false);
+        alert(json.Error);
       }
     } catch (error) {
       console.log(error);
     }
   };
-
-  const onclickMovie = (id) => {
-    navigate(`/movie/${id}`)
-  }
   
-  return(
+// 무한스크롤
+  useEffect(() => {
+    console.log("IntersectionObserver")
+    const observer = new IntersectionObserver(
+      (entries) => {
+        // 화면아래 내렸을때 동작
+        if (entries[0].isIntersecting && hasNextPage) {
+          Pagee.current++;
+          sendApi(Pagee.current); 
+        }
+      },
+    );
+
+    // 화면아래 영역을 감시하도록한다.
+    if (observerTargetEl.current) {
+      observer.observe(observerTargetEl.current);
+    }
+  
+    return () => {
+      // 영역 감시 해제 
+      observer.disconnect();
+    };
+  }, [hasNextPage]);
+
+  //상세페이지 이동 
+  const onclickMovie = (value) => {
+    setValue(value);
+    navigate(`/movie/${value}`)
+  }
+
+    return(
     <>
-    <div className={st('Resultcontainer')}>
-      <div className={st('Resultcontainer-box')}>
-      {dataState ? (  
-        <div className={st('Result-container__inner')}>
-          <div className={st('Result-container__movies')}>
-          {loading ? <Loading/>: 
-            dataList.map((movie) => (
-              <div className={st('Result-container__MovieItem')} 
-                onClick={() => onclickMovie(movie.imdbID)} key={movie.imdbID} 
-                style={{'--props-image': `url(${movie.Poster})`}}>
-                <div className={st('MovieItem-info')}>
-                  <div className="year">{movie.Year}</div>
-                  <div className="title">{movie.Title}</div>
-                </div>
-              </div>
-            ))
-          }   
+      <div className={st('Resultcontainer')}>
+        <div className={st('Resultcontainer-box')}>
+        {dataState ? (  
+          <div className={st('Result-container__inner')}>
+            <div className={st('Result-container__movies')}>
+            {
+              (loading ? Array.from(new Array(10)) : dataList).map((movie, index) => (
+                // 스켈레톤 적용. movie 데이터가 있을때?
+                movie ? (
+                  <div className={st('Result-container__MovieItem')}
+                    onClick={() => onclickMovie(movie.imdbID)} key={`${movie.imdbID}`} 
+                    style={{'--props-image': movie.Poster !== 'N/A' ? `url(${movie.Poster})` : 'url(https://joonfont.com/wp-content/uploads/2019/07/notdef2.jpg)',}}>
+                    <div className={st('MovieItem-info')}>
+                      <div className="year">{movie.Year}</div>
+                      <div className="title">{movie.Title}</div>
+                    </div>
+                  </div>
+                ) : (
+                  // 없을때 
+                  <div className={st('Result-container__MovieItem')} >
+                    <Skeleton variant="rectangular" width={200} height={300} animation="wave"/>
+                  </div>
+                )
+              ))
+            }   
+            </div>
+            <div className={st('Result-container__next')} ref={observerTargetEl}>
+              {loading ? <Loading/> : ""}
+            </div>
           </div>
-        </div>
-      ) : (
-        <div className={st('NotFoundLayer')}>
-          <div className={st('NotFoundLayer-title')}>
-            Search for the movie title!
+          ) : (
+          <div className={st('NotFoundLayer')}>
+            <div className={st('NotFoundLayer-title')}>
+              Search for the movie title!
+            </div>
           </div>
+          )
+        }  
         </div>
-        )
-      }  
       </div>
-    </div>
     </>
   )
 };
