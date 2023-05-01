89f035191acb7c5969ccf34a007fd529a30e1b70
hahahaday12 <hahahaday12@naver.com>
2023-05-02 03:59:01 +0900

    feat: 상세 페이지 상태 공유를 위해 로직 추가
    - store 에서 만든 contextapi를 이용하여 header  movie 컴포넌트에 값 전달

--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -1,18 +1,23 @@
-import React  from "react"
+import React, { useContext }  from "react"
 import { Link, NavLink } from 'react-router-dom';
 import '../../scss/components/common/header.scss'
-import CustomNavLink from './customNavLink';
+import PageContext from "../../store/context";
+
+
 function Header() {
+
+  const { value } = useContext(PageContext);
+
   return(
     <>
     <div className="header">
       <div className="header-category__box">
-        <Link Link to="/" className="header__logo">
+        <Link to="/" className="header__logo">
           <span>OMDbAPI</span>.COM
         </Link>
         <div className="header-category__nave">
           <NavLink exact to="/" activeClassName="active">Search</NavLink>
-          <CustomNavLink to="/movie/tt0978759" childeren={"Movie"}></CustomNavLink>
+          <NavLink to={`/movie/${value}`}>Movie</NavLink>
           <NavLink to="/about" activeClassName="active">About</NavLink>            
         </div> 
       </div>
