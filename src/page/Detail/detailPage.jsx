7a911be014526300043dc23f801d57e56fd51799
hahahaday12 <hahahaday12@naver.com>
2023-05-02 04:00:54 +0900

    feat: 스켈레톤 컴포넌트 추가
    - 로딩시에 보여질 부분 컴포넌트 추가

--- src/page/Detail/detailPage.jsx
+++ src/page/Detail/detailPage.jsx
@@ -2,13 +2,12 @@ import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react';
 import styles from '../../scss/Detail/detail.module.scss'
 import classNames from 'classnames/bind'
-import Loading from '../../components/loading';
-
+import DetailSkeleton from './component/skelaton';
 
 function DetailPage () {
 
   const {id} = useParams();
-  const [movieimg, setMovieimg] = useState([]);
+  const [movie, setMovie] = useState([]);
   const [loading, setLoading] = useState(true);
   const [data, useData] = useState([]);
   const st = classNames.bind(styles)
@@ -19,9 +18,8 @@ function DetailPage () {
       const res = await fetch(
         `https://omdbapi.com/?apikey=7035c60c&i=${id}&plot=full`
       )
-      // .then(response => response.json())
       const json = await res.json();
-      setMovieimg(json);
+      setMovie(json);
       useData(json.Ratings);
       setLoading(false)
     }
@@ -29,71 +27,70 @@ function DetailPage () {
   },[id]); 
 
   return(
-    <>
-    {/* {loading ? <Loading/>: null}  */}
-    {/* <Loading/> */}
-    
-    { loading ? <div className={st('Load')}><Loading/></div> : 
+    <>  
     <div className={st('Detail-Container')}>
       <div className={st('Detail-Container_box')}>
         <div className={st('Detail-Container_inner')}>
         
-        <div className={st('Detail-Container__moviebox')} delay={4000}>
-          <img className={st('Detail-Container__movieimg')} 
-          src={String(movieimg.Poster).replace('SX300', 'SX700')}/>   
-        </div>
-       
-        <div className={st('Detail-Container__infobox')}>
-          <div className={st('Detail-infobox__title')}>
-              <span>{movieimg.Title}</span>
+          { loading ? < DetailSkeleton /> : (
+            <>
+          <div className={st('Detail-Container__moviebox')}>
+            <img className={st('Detail-Container__movieimg')} 
+            src={movie.Poster !== 'N/A' ? movie.Poster.replace('SX300', 'SX700'): "https://joonfont.com/wp-content/uploads/2019/07/notdef2.jpg"}/>   
           </div>
 
-          <div className={st('Detail-infobox__label')}>
-            <span>{movieimg.Released} • {movieimg.Runtime} • {movieimg.Country}</span>
-          </div>
+          <div className={st('Detail-Container__infobox')}>
+            <div className={st('Detail-infobox__title')}>
+                <span>{movie?.Title}</span>
+            </div>
 
-          <div className={st('Detail-infobox__plot')}>
-            <span>{movieimg.Plot}</span>
-          </div>
-       
-          <div className={st('Detail-infobox__ratings')}>
-            <h3>Ratings</h3>
-     
-              <div className={st('Detail-Database__box')}>
-                {data.map((obj) => (
-                  <div title={obj.Source} className={st('Detail-Movie__Database')}>
-                    <img src={`https://raw.githubusercontent.com/ParkYoungWoong/vue3-movie-app/master/src/assets/${obj.Source}.png`} alt={obj.Source} />
-                    <span>{obj.Value}</span>
-                  </div> 
-                ))
-                }
-              </div>
-          </div>
+            <div className={st('Detail-infobox__label')}>
+              <span>{movie.Released} • {movie.Runtime} • {movie.Country}</span>
+            </div>
 
-          <div className={st('Detail-infobox__actors')}>
-            <h3>Actor</h3>
-            <span>{movieimg.Actors}</span>
-          </div>
+            <div className={st('Detail-infobox__plot')}>
+              <span>{movie.Plot}</span>
+            </div>
+        
+            <div className={st('Detail-infobox__ratings')}>
+              <h3>Ratings</h3>
+      
+                <div className={st('Detail-Database__box')}>
+                  {data.map((obj) => (
+                    <div title={obj.Source} className={st('Detail-Movie__Database')}>
+                      <img src={`https://raw.githubusercontent.com/ParkYoungWoong/vue3-movie-app/master/src/assets/${obj.Source}.png`} alt={obj.Source} />
+                      <span>{obj.Value}</span>
+                    </div> 
+                  ))
+                  }
+                </div>
+            </div>
 
-          <div className={st('Detail-infobox__actors')}>
-            <h3>Director</h3>
-            <span>{movieimg.Director}</span>
-          </div>
+            <div className={st('Detail-infobox__actors')}>
+              <h3>Actor</h3>
+              <span>{movie.Actors}</span>
+            </div>
 
-          <div className={st('Detail-infobox__actors')}>
-            <h3>Production</h3>
-            <span>{movieimg.Production}</span>
-          </div>
+            <div className={st('Detail-infobox__actors')}>
+              <h3>Director</h3>
+              <span>{movie.Director}</span>
+            </div>
 
-          <div className={st('Detail-infobox__actors')}>
-            <h3>Genre</h3>
-            <span>{movieimg.Genre}</span>
+            <div className={st('Detail-infobox__actors')}>
+              <h3>Production</h3>
+              <span>{movie.Production}</span>
+            </div>
+
+            <div className={st('Detail-infobox__actors')}>
+              <h3>Genre</h3>
+              <span>{movie.Genre}</span>
+            </div>
           </div>
-        </div>
+          </>
+          )}
         </div>  
       </div>
     </div>
-     }
     </>
   )
 };
